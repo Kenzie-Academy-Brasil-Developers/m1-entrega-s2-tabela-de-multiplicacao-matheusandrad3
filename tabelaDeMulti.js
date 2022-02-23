9e3108168ae3daf8220b83e35710fee551b6fc27
matheusandrad3 <matheusandradedesouza@outlook.com>
2022-02-23 18:31:42 -0300

    atualização do nome html

--- tabelaDeMulti.js
+++ tabelaDeMulti.js
@@ -2,14 +2,17 @@ function tabelaDeMultiplicacao(numero){
     let resultadoTotal = [];
 
 for (let i = 0; i <= numero; i ++){
-    resultadoTotal[i] =[]
+    resultadoTotal[i] =[];
     for (let j = 0; j <= numero; j++){ 
-     resultadoTotal[i][j] = i * j 
+     resultadoTotal[i][j] = i * j; 
     }
 }
-
     return resultadoTotal;
 }
-
-let result = tabelaDeMultiplicacao (10);
+let result = tabelaDeMultiplicacao(10);
 console.table(result);
+
+
+
+
+
